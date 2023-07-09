dc3c1cf9c5667e8f46c08e6929f98208c5e5abc9
201496441 <magorothabo1@gmail.com>
2023-07-09 10:25:27 -0700

    deploy all

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# a Fabric script (based on the file 2-do_deploy_web_static.py) that creates
+# and distributes an archive to your web servers, using the function deploy:
 import os.path
 from datetime import datetime
 from fabric.api import env
@@ -6,18 +8,14 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['174.129.55.197', '100.26.53.83']
+env.hosts = ["174.129.55.197", "100.26.53.83"]
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """Creates an archive file of the directory web_static directory"""
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
@@ -27,14 +25,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
+    """Sends an archive to a web server"""
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -68,7 +59,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """Create and distribute an archive to a web server."""
+    """Creates and sends an archive to a web server."""
     file = do_pack()
     if file is None:
         return False
