8139769412b70c865dc086e0fcb9dd8a1a5f8761
201496441 <magorothabo1@gmail.com>
2023-08-17 07:55:10 +0000

    task1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return render_template("8-cities_by_states.html")
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
